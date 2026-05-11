1a3faeb8f0ac0bb8531b931827d9d67e7a543985
Vamsi <mkrishnadev1996@gmail.com>
2026-05-11 18:07:48 +0530

    first commit

---
+++ learning1/main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel, Field
+from enum import Enum
+
+app =FastAPI()
+class InputModel(BaseModel):
+    name:str = Field()
+
+class StudentClass(int ,Enum):
+    one = 1,
+    two = 2,
+    three = 3,
+    four = 4,
+    five = 5,
+    six = 6,
+    seven = 7,
+    eight = 8,
+    nine = 9,
+    ten = 10
+
+
+
+students ={
+    1:{"name":"John",
+       "age":12,
+       "class":5},
+    2:{"name":"Missy",
+       "age":10,
+       "class":3},
+    3:{"name":"Sara",
+       "age":13,
+       "class":6},
+    4:{"name":"Ken",
+       "age":12,
+       "class":5},
+}
+
+@app.get("/")
+def index():
+    return {'message':'Hello world. Refer docs at path /docs'}
+
+@app.get("/get-student-by-id/{student_id}")
+async def get_student_by_id(student_id: int = Path(..., description="Enter Id of student", gt=0, lt=10)):
+    if student_id not in students:
+        return {"Error":"Id not found"}
+    return students[student_id]
+
+@app.get("/get-student-by-name")
+async def get_student_by_name(name : str = Query(...,description="Name of the student")):
+    for id in students.keys():
+        if students[id]["name"].lower()==name.lower() :
+            return students[id]
+    return {"Error":"Name not found"}
+
+@app.get("/get-students-in-class/{student_class}")
+async def get_students_in_class(student_class :StudentClass = Path(...,description="Class for which list of students are required")):
+    result = []
+
+    for student in students.values():
+        if student["class"] == student_class.value:
+            result.append(student)
+
+    if not result:
+        return {"Error": "No students found in this class"}
+
+    return {"class":student_class,"data":result}
